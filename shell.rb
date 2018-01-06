fd06b8eaaff1627fafe1493eeb9c4a31d4091496
Sedgewick <yxin.pu@gmail.com>
2018-01-06 23:50:43 +0800

    Fix: 修正了「不能同步新建文件」的 bug &「文件名不能含空格」的 bug 。

--- shell.rb
+++ shell.rb
@@ -1,6 +1,8 @@
 require 'json'
 require 'pp'
 
+DEBUG = false
+
 class MD5s
   attr_reader :md5s
   
@@ -19,31 +21,42 @@ class MD5s
   def get_all_md5(directory)
     Dir.foreach(directory) do |subdir|
       next if @ignored_dirs.include? subdir # .scan(/(?<=\/)[\w\.]+$/).first
+      dir = directory + '/' + subdir
+      if /\s/ =~ subdir
+        new_dir = directory + '/' + subdir.gsub!(/\s/, '_')
+        File.rename(dir, new_dir)
+        dir = new_dir
+      end
+      
       if /\./ =~ subdir
-        # p "file: #{subdir}"
-        dir = directory + '/' + subdir
-        md5s << {
-                  :file_name => dir,
-                  :md5 => get_md5(dir)
-                }
+        DEBUG && puts("file: #{dir}")
+        @md5s << {
+                   file_name: dir,
+                   md5: get_md5(dir)
+                 }
       else
-        # p "dir: #{subdir}"
-        get_all_md5(directory + '/' + subdir)
+        DEBUG && puts("dir:  #{dir}")
+        get_all_md5(dir)
       end
     end
+    @md5s
   end
   
   def check_diff
     @updated_files = []
     f = File.open('md5s.json', 'r').read
     json = JSON.parse(f)
-    json.each do |file|
-      fname = file['file_name']
-      now_md5 = get_md5(fname)
-      unless now_md5 == file['md5']
-        # puts "#{fname} UPDAT!"
-        file['md5'] = now_md5
+    @md5s.each do |file|
+      fname = file[:file_name]
+      old_file = json.select { |e| e['file_name'] == fname }
+      if old_file == []
         @updated_files << fname
+      else
+        old_md5 = old_file.first['md5']
+        unless old_md5 == file[:md5]
+          puts "#{fname} UPDAT!"
+          @updated_files << fname
+        end
       end
     end
     
@@ -70,7 +83,8 @@ class MD5s
       when /n|no/
         puts "😒"
       else
-        puts "wrong answer😒"
+        answers = ["wrong answer😒", "丫在調戲我嗎🤯", "能不能認真點😤"]
+        puts answers[rand(answers.size)]
       end
     end
   end
